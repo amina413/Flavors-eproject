0bf3bcec3805d3aaf062c654b5841d11991db470
vscodefiles <kosuagwu4r@gmail.com>
2025-02-24 16:23:20 +0100

    Added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Spices', 'Herbs', 'Clove', 'Cinnamon', 'Garlic', 'Nutmeg'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+  });
+  
