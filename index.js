ec0ba1f1307734cfe285d0a866b492586894ea9d
KSAMUELGIF <kosuagwu4r@gmail.com>
2025-02-25 21:44:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const typed = new Typed('.multiple-text', {
     loop: true,
   });
   
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const shopLink = document.getElementById("shop-link");
+    const dropdown = document.querySelector(".dropdown");
+
+    shopLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        dropdown.classList.toggle("active");
+    });
+
+    // Close dropdown when clicking outside
+    document.addEventListener("click", function (event) {
+        if (!dropdown.contains(event.target) && event.target !== shopLink) {
+            dropdown.classList.remove("active");
+        }
+    });
+});
